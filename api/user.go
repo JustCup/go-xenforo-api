cf6a15741af0f5670b0b5278aa5e6a19dc648102
JustCup <32245758+JustCup@users.noreply.github.com>
2020-05-24 17:21:55 +0300

    UsersGetID add object

--- api/user.go
+++ api/user.go
@@ -10,7 +10,7 @@ import (
 // Gets information about the specified user.
 //
 // https://xenforo.com/community/pages/api-endpoints/#route_get_users_id_
-func (xf *XF) UsersGetID(id int, params Params) (response object.User, err error) {
+func (xf *XF) UsersGetID(id int, params Params) (response object.UsersGet, err error) {
 	err = xf.RequestUnmarshal("GET", fmt.Sprintf("%s/%d/", "users", id), params, &response)
 	return
 }
--- object/user.go
+++ object/user.go
@@ -69,3 +69,15 @@ type UserFindName struct {
 	Exact           *User  `json:"exact,omitempty"` // The user that matched the given username exactly. Empty struct or user info. TODO: refactor(make NULL great again)
 	Recommendations []User `json:"recommendations"` // A list of users that match the prefix of the username (but not exactly)
 }
+
+// ProfilePosts struct.
+type ProfilePosts struct {
+	//
+}
+
+// UsersGet stuct.
+type UsersGet struct {
+	User         User           `json:"user"`
+	ProfilePosts []ProfilePosts `json:"profile_posts,omitempty"` // List of profile posts on the requested page
+	Pagination   Pagination     `json:"pagination,omitempty"`    // Pagination details
+}
