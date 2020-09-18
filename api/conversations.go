be91d40ab9800b5c4d26ca4df069948c5a5c35fe
JustCup <cupjust@gmail.com>
2020-09-18 16:37:04 +0300

    intermediate update

---
+++ api/conversations.go
@@ -0,0 +1,46 @@
+package api // import "github.com/JustCup/go-xenforo-api/api"
+import (
+	"fmt"
+
+	"github.com/JustCup/go-xenforo-api/object"
+)
+
+// ConversationMessagesSend funciton.
+//
+// Replies to a conversation
+//
+// https://xenforo.com/community/pages/api-endpoints/#route_post_conversation_messages_
+func (xf *XF) ConversationMessagesSend(params Params) (response object.ConversationMessagesResponse, err error) {
+	err = xf.RequestUnmarshal("POST", "conversation-messages", params, &response)
+	return
+}
+
+// ConversationMessagesGet funciton.
+//
+// Gets the specified conversation message.
+//
+// https://xenforo.com/community/pages/api-endpoints/#route_post_conversation_messages_
+func (xf *XF) ConversationMessagesGet(id int) (response object.ConversationMessagesResponse, err error) {
+	err = xf.RequestUnmarshal("GET", fmt.Sprintf("conversation-messages/%d/", id), Params{}, &response)
+	return
+}
+
+// ConversationsGet funciton.
+//
+// Gets the API user's list of conversations.
+//
+// https://xenforo.com/community/pages/api-endpoints/#route_get_conversations_
+func (xf *XF) ConversationsGet(params Params) (response object.ConversationsResponse, err error) {
+	err = xf.RequestUnmarshal("GET", "conversations", params, &response)
+	return
+}
+
+// ConversationsGetID funciton.
+//
+// Gets information about the specified conversation.
+//
+// https://xenforo.com/community/pages/api-endpoints/#route_get_conversations_id_
+func (xf *XF) ConversationsGetID(id int) (response object.ConversationsResponse, err error) {
+	err = xf.RequestUnmarshal("GET", fmt.Sprintf("conversations/%d/", id), Params{}, &response)
+	return
+}
---
+++ object/conversations.go
@@ -0,0 +1,58 @@
+package object // import "github.com/JustCup/go-xenforo-api/object"
+
+// Conversation struct.
+type Conversation struct {
+	Username            string      `json:"username"`
+	Recipients          interface{} `json:"recipients"`
+	IsStarted           bool        `json:"is_starred"`
+	CanEdit             bool        `json:"can_edit"`
+	CanReply            bool        `json:"can_reply"`
+	CanInvite           bool        `json:"can_invite"`
+	CanUploadAttachment bool        `json:"can_upload_attachment"`
+	ConversationID      int         `json:"conversation_id"`
+	Title               string      `json:"title"`
+	UserID              int         `json:"user_id"`
+	StartDate           int         `json:"start_date"`
+	OpenInvite          bool        `json:"open_invite"`
+	ConversationOpen    bool        `json:"conversation_open"`
+	ReplyCount          int         `json:"reply_count"`
+	RecipientCount      int         `json:"recipient_count"`
+	FirstMessageID      int         `json:"first_message_id"`
+	LastMessageDate     int         `json:"last_message_date"`
+	LastMessageID       int         `json:"last_message_id"`
+	LastMessageUserID   int         `json:"last_message_user_id"`
+	Starter             User        `json:"Starter"`
+}
+
+// ConversationMessage struct.
+type ConversationMessage struct {
+	Username          string        `json:"username"`
+	CanEdit           bool          `json:"can_edit"`
+	CanReact          bool          `json:"can_react"`
+	Conversation      Conversation  `json:"Conversation"`        // Conditionally returned. If requested by context, the conversation this message is part of.
+	Attachments       []interface{} `json:"Attachments"`         // Conditionally returned. If there are attachments to this message, a list of attachments.
+	IsReactedTo       bool          `json:"is_reacted_to"`       // True if the viewing user has reacted to this content.
+	VisitorReactionID int           `json:"visitor_reaction_id"` // If the viewer reacted, the ID of the reaction they used.
+	MessageID         int           `json:"message_id"`
+	ConversationID    int           `json:"conversation_id"`
+	MessageDate       int           `json:"message_date"`
+	UserID            int           `json:"user_id"`
+	Message           int           `json:"message"`
+	AttachCount       int           `json:"attach_count"`
+	ReactionScore     int           `json:"reaction_score"`
+	User              User          `json:"User"`
+}
+
+// ConversationMessagesResponse sturct.
+type ConversationMessagesResponse struct {
+	Success bool                `json:"success,omitempty"`
+	Message ConversationMessage `json:"message"`
+}
+
+// ConversationsResponse sturct.
+type ConversationsResponse struct {
+	Success      bool                  `json:"success,omitempty"`
+	Conversation Conversation          `json:"conversation"`
+	Messages     []ConversationMessage `json:"messages"`
+	Pagination   Pagination            `json:"pagination"`
+}
