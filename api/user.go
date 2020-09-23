102350ce31852e34a2021772e3fd4908c9f659e9
JustCup <cupjust@gmail.com>
2020-09-23 16:03:39 +0300

    add user edit

--- api/user.go
+++ api/user.go
@@ -9,7 +9,7 @@ import (
 //
 // Gets a list of users (alphabetically).
 //
-// https://xenforo.com/community/pages/api-endpoints/#route_get_users_id_
+// https://xenforo.com/community/pages/api-endpoints/#route_get_users_
 func (xf *XF) UsersGet(params Params) (response object.UsersGet, err error) {
 	err = xf.RequestUnmarshal("GET", "users", params, &response)
 	return
@@ -42,5 +42,15 @@ func (xf *XF) UsersFindName(params Params) (response object.UserGetFindName, err
 // https://xenforo.com/community/pages/api-endpoints/#route_get_users_id_
 func (xf *XF) UsersGetID(id int, params Params) (response object.UsersGetID, err error) {
 	err = xf.RequestUnmarshal("GET", fmt.Sprintf("%s/%d/", "users", id), params, &response)
+	return
+}
+
+// UsersEdit funciton.
+//
+// Updates an existing user.
+//
+// https://xenforo.com/community/pages/api-endpoints/#route_post_users_id_
+func (xf *XF) UsersEdit(id int, params Params) (response object.UsersGetID, err error) {
+	err = xf.RequestUnmarshal("POST", fmt.Sprintf("%s/%d/", "users", id), params, &response)
 	return
 }
