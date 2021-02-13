70fb4094bc74f3d3dbf18376f2fbed4c3e305dd0
JustCup <cupjust@gmail.com>
2021-02-13 16:43:06 +0300

    update "forums" method.

--- api/forum.go
+++ api/forum.go
@@ -6,22 +6,32 @@ import (
 	"github.com/JustCup/xenforo-sdk/object"
 )
 
-// ForumsGet function.
+// Forums function.
 //
 // Gets information about the specified forum.
 //
 // https://xenforo.com/community/pages/api-endpoints/#route_get_forums_id_
-func (xf *XF) ForumsGet(id int, params Params) (response object.ForumsGet, err error) {
+func (xf *XF) Forums(id int, params Params) (response object.ForumsResponse, err error) {
 	err = xf.RequestUnmarshal("GET", fmt.Sprintf("%s/%d/", "forums", id), params, &response)
 	return
 }
 
-// ForumsGetThreads function.
+// ForumsMarkRead function.
+//
+// Marks the forum as read up until the specified time. This cannot mark a forum as unread.
+//
+// https://xenforo.com/community/pages/api-endpoints/#route_post_forums_id_mark_read
+func (xf *XF) ForumsMarkRead(id int, params Params) (response object.ForumsResponse, err error) {
+	err = xf.RequestUnmarshal("GET", fmt.Sprintf("%s/%d/%s", "forums", id, "mark-read"), params, &response)
+	return
+}
+
+// ForumsThreads function.
 //
 // Gets a page of threads from the specified forum.
 //
 // https://xenforo.com/community/pages/api-endpoints/#route_get_forums_id_
-func (xf *XF) ForumsGetThreads(id int, params Params) (response object.ForumsGetThreads, err error) {
+func (xf *XF) ForumsThreads(id int, params Params) (response object.ForumsResponse, err error) {
 	err = xf.RequestUnmarshal("GET", fmt.Sprintf("%s/%d/%s/", "forums", id, "threads"), params, &response)
 	return
 }
--- object/forums.go
+++ object/forums.go
@@ -2,23 +2,17 @@ package object // import "github.com/JustCup/xenforo-sdk/object"
 
 // Forum struct.
 type Forum struct {
-	AllowPosting  bool `json:"allow_posting"`
-	AllowPool     bool `json:"allow_pool"`
-	RequirePrefix bool `json:"require_prefix"`
-	MinTags       int  `json:"min_tags"`
+	ForumTypeID   string `json:"forum_type_id"`
+	AllowPosting  bool   `json:"allow_posting"`
+	RequirePrefix bool   `json:"require_prefix"`
+	MinTags       uint   `json:"min_tags"`
 }
 
-// ForumsGet struct.
-type ForumsGet struct {
+// ForumsResponse struct.
+type ForumsResponse struct {
 	Forum      Forum      `json:"forum"`
 	Threads    []Thread   `json:"threads"`    // Threads on this page
 	Pagination Pagination `json:"Pagination"` // Pagination information
 	Sticky     []Thread   `json:"sticky"`     // If on page 1, a list of sticky threads in this forum. Does not count towards the per page limit.
-}
-
-// ForumsGetThreads struct.
-type ForumsGetThreads struct {
-	Threads    []Thread   `json:"threads"`    // Threads on this page
-	Pagination Pagination `json:"pagination"` // Pagination information
-	Sticky     []Thread   `json:"sticky"`     // If on page 1, a list of sticky threads in this forum. Does not count towards the per page limit.
+	Success    bool       `json:"success"`
 }
