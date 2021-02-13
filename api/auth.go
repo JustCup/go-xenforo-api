6e251b91a8bc807bad06b7dc2f7d7976dd1b0444
JustCup <cupjust@gmail.com>
2021-02-13 15:54:09 +0300

    update "auth" method.

--- api/auth.go
+++ api/auth.go
@@ -5,10 +5,39 @@ import (
 
 // Auth function.
 //
-// Tests a login and password for validity. Only available to super user keys.
+// Tests a login and password for validity.
+//
+// Only available to super user keys.
 //
 // https://xenforo.com/community/pages/api-endpoints/#route_post_auth_
 func (xf *XF) Auth(params Params) (response object.AuthResponse, err error) {
 	err = xf.RequestUnmarshal("POST", "auth", params, &response)
+	return
+}
+
+// AuthFromSession function.
+//
+// Looks up the active XenForo user based on session ID or remember cookie value.
+// This can be used to help with seamless SSO with XF, assuming the session or remember cookies are available to your page.
+// At least one of session_id and remember_cookie must be provided.
+//
+// Only available to super user keys.
+//
+// https://xenforo.com/community/pages/api-endpoints/#route_post_auth_from_session
+func (xf *XF) AuthFromSession(params Params) (response object.AuthResponse, err error) {
+	err = xf.RequestUnmarshal("POST", "auth/from-session", params, &response)
+	return
+}
+
+// AuthLoginToken function.
+//
+// Generates a token that can automatically log into a specific XenForo user when the login URL is visited.
+// If the visitor is already logged into a XenForo account, they will not be logged into the specified account.
+//
+// Only available to super user keys.
+//
+// https://xenforo.com/community/pages/api-endpoints/#route_post_auth_login_token
+func (xf *XF) AuthLoginToken(params Params) (response object.AuthResponse, err error) {
+	err = xf.RequestUnmarshal("POST", "auth/login-token", params, &response)
 	return
 }
--- object/auth.go
+++ object/auth.go
@@ -2,5 +2,9 @@ package object // import "github.com/JustCup/xenforo-sdk/object"
 
 // AuthResponse struct.
 type AuthResponse struct {
-	User User `json:"user"`
+	User       User   `json:"user"`    // If successful, the user record of the matching user.
+	Success    bool   `json:"success"` // If false, no session or remember cookie could be found.
+	LoginToken string `json:"login_token"`
+	LoginURL   string `json:"login_url"`   // Direct user to this URL to trigger a login.
+	ExpiryDate uint32 `json:"expiry_date"` // Unix timestamp of when the token expires. An error will be displayed if the token is expired or invalid.
 }
